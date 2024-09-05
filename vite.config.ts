432665411d841d9c1ec912fed742b0e9594010ee
Ryan Palermo <rpalermodrums@gmail.com>
2024-09-05 14:38:41 -0400

    update vite config for testing and static assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    exclude: [...configDefaults.exclude, 'src/hooks/useParticleBackground.tsx'],
   },
 })
