5afe53ec6902befaace267e75f933aa6d1f3a57d
Ryan Palermo <rpalermodrums@gmail.com>
2024-09-05 12:56:40 -0400

    (hopefully) add assets to production build output

--- src/components/MarkdownView.tsx
+++ src/components/MarkdownView.tsx
@@ -1,23 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
+import markdownContent from '../assets/posts/v1__20240905__music-tech-relationship.md?raw';
 
 const MarkdownView: React.FC = () => {
-  const [markdown, setMarkdown] = useState('');
-  const { id } = useParams<{ id: string }>();
-
-  useEffect(() => {
-    fetch('/src/assets/posts/v1__20240905__music-tech-relationship.md')
-      .then(response => response.text())
-      .then(text => setMarkdown(text))
-      .catch(error => console.error('Error fetching markdown:', error));
-  }, [id]);
+  const { slug } = useParams<{ slug: string }>();
+  console.log(slug);
 
   return (
     <>
-      <h3 className="text-2xl font-bold mb-4">***TODO: PARSE AND RENDER ACTUAL MARKDOWN FILE***</h3>
+      <h3 className="text-2xl font-bold mb-4">Markdown Content</h3>
       <div className="container mx-auto px-4 py-8">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+        <ReactMarkdown>{markdownContent}</ReactMarkdown>
       </div>
     </>
   );
