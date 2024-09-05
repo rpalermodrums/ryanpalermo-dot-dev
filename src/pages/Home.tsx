46e336f5f8239733d6bea8eb2b9c0c2c3437718d
Ryan Palermo <rpalermodrums@gmail.com>
2024-09-04 23:58:01 -0400

    sorry sorry sorry we can deploy now

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,6 +27,7 @@ const Home: React.FC = () => {
     }
 
     function animate() {
+      if (!ctx || !canvas) return;
       requestAnimationFrame(animate);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
