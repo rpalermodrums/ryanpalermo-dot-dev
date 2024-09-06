8eb0854a39113052cb02ee14fcbf31ffc6ee45ab
Ryan Palermo <rpalermodrums@gmail.com>
2024-09-05 20:59:51 -0400

    add redirect on mount when a `next` query param exists

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Thoughts from './pages/Thoughts'
 import Contact from './pages/Contact'
 import MarkdownView from './components/MarkdownView'
+import { useEffect } from 'react';
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const nextRoute = searchParams.get('next');
+
+    if (nextRoute) {
+      navigate(nextRoute, { replace: true });
+    }
+  }, []);
+
   return (
     <Router>
       <Layout>
